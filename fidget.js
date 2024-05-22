b1808738427f362abadf84ef3c35e0ee073036e0
Anthony Kelley <kelley.anthonyk@gmail.com>
2024-05-21 20:39:18 -0500

    update for today, more to come

---
+++ fidget.js
@@ -0,0 +1,18 @@
+// let position = 0;
+
+// function prev(){
+//     console.log(" left SCroll");
+    // if (position <= 0) {
+    //     var carousel = document.getElementById("carouselExampleFade");
+    //     position +=1;
+    //     carousel.style.transform = translate
+    //     var offset = position * 200 + 4;
+    //     "translateX(-" + offset + "px)";
+    // }
+
+
+// }
+
+// function scrollLeft(){
+// console.log("scroll Left");
+// }
